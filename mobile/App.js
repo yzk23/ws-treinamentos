dd66589e2d2385537a871b1156eb1b4789a047e9
yzk23 <isaacoelhomedeiros@gmail.com>
2020-07-04 22:44:30 -0300

    StatusBar branca

--- mobile/App.js
+++ mobile/App.js
@@ -6,7 +6,7 @@ import Routes from './src/routes';
 export default function App() {
   return (
     <React.Fragment>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <StatusBar backgroundColor="transparent" translucent />
       <Routes />
     </React.Fragment>
   );
